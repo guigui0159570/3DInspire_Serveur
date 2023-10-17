874d78d708cf85eebe80adb1f5bd369deb7e0c6b
Théo Bernaville <Theo.Bernaville@uphf.fr>
2023-10-17 14:25:24 +0200

    Squelette serveur

---
+++ src/test/java/com/example/_3dinspire_serveur/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._3dinspire_serveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
