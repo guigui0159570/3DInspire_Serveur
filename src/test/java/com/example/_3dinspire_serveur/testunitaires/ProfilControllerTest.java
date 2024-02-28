bdc23c0c3a4715242d8afc214191007cbcbb4c70
Thomas1406 <Thomas.Delacroix@uphf.fr>
2024-02-28 20:24:52 +0100

    update tests
    update tests

--- src/test/java/com/example/_3dinspire_serveur/testunitaires/ProfilControllerTest.java
+++ src/test/java/com/example/_3dinspire_serveur/testunitaires/ProfilControllerTest.java
@@ -8,9 +8,11 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -60,7 +62,8 @@ public class ProfilControllerTest {
         var request = MockMvcRequestBuilders.get("/profile");
 
         mockMvc.perform(request)
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
